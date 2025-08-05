e4ffb3fc77ea645530424c80762f82818f453f01
Samsan <nobertsam1812@gmail.com>
2025-08-05 18:57:42 +0530

    Vite.js file has edited

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,4 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import pkg from "vite";
-// const { createFilter } = pkg;
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
